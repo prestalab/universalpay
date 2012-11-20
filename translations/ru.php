af62af5d6b49f9e681d7f3e8d6ba67a22f35a6ed
prestalab <prestalabx@gmail.com>
2012-11-20 11:56:26 +0400

    Updated russian translation

--- translations/ru.php
+++ translations/ru.php
@@ -28,15 +28,14 @@ $_MODULE['<{universalpay}prestashop>adminunipaysystem_14c5920c988429689db102b62f
 $_MODULE['<{universalpay}prestashop>adminunipaysystem_38fb7d24e0d60a048f540ecb18e13376'] = 'Сохранить';
 $_MODULE['<{universalpay}prestashop>universalpay_6d38dff93844d839c18ff54b312d327b'] = 'Универсальный модуль оплаты';
 $_MODULE['<{universalpay}prestashop>universalpay_4135c7ed7be4a58bafe97bbc98c191e9'] = 'Создает методы оплаты';
-$_MODULE['<{universalpay}prestashop>universalpay_a82be0f551b8708bc08eb33cd9ded0cf'] = 'Информация';
-$_MODULE['<{universalpay}prestashop>universalpay_34b6cd75171affba6957e308dcbd92be'] = 'Версия';
-$_MODULE['<{universalpay}prestashop>universalpay_794df3791a8c800841516007427a2aa3'] = 'Лицензия';
-$_MODULE['<{universalpay}prestashop>universalpay_672caf27f5363dc833bda5099775e891'] = 'Разработчик';
-$_MODULE['<{universalpay}prestashop>universalpay_b5a7adde1af5c87d7fd797b6245c2a39'] = 'Описание';
-$_MODULE['<{universalpay}prestashop>universalpay_e4a14cd4f30612b9a8ad8f6aa5887a6f'] = 'Модули для PrestaShop';
-$_MODULE['<{universalpay}prestashop>universalpay_254f642527b45bc260048e30704edb39'] = 'Настройки';
+$_MODULE['<{universalpay}prestashop>universalpay_c9cc8cce247e49bae79f15173ce97354'] = 'Сохранить';
+$_MODULE['<{universalpay}prestashop>universalpay_f4f70727dc34561dfde1a3c529b6205c'] = 'Настройки';
 $_MODULE['<{universalpay}prestashop>universalpay_94ab57ecb6cd678b5a60cafb8d86dbc1'] = 'Добавьте методы оплаты на';
 $_MODULE['<{universalpay}prestashop>universalpay_522fbb7b9cfeab6672777c23cc4eac8d'] = 'вкладке Платежи>Платежные системы';
+$_MODULE['<{universalpay}prestashop>universalpay_a599c92bfe99c30fe14bee3a9ec90253'] = 'Кнопка подтверждения';
+$_MODULE['<{universalpay}prestashop>universalpay_1e942b304ac381c702b1e20c42a448ef'] = 'Кнопка подтверждения на странице списка платежных систем';
+$_MODULE['<{universalpay}prestashop>universalpay_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Включено';
+$_MODULE['<{universalpay}prestashop>universalpay_b9f5c797ebbf55adccdd8539a65a0241'] = 'Отключено';
 $_MODULE['<{universalpay}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'Этот способ оплаты недоступен';
 $_MODULE['<{universalpay}prestashop>form_a82be0f551b8708bc08eb33cd9ded0cf'] = 'Информация';
 $_MODULE['<{universalpay}prestashop>form_34b6cd75171affba6957e308dcbd92be'] = 'Версия';
@@ -49,3 +48,5 @@ $_MODULE['<{universalpay}prestashop>payment_execution_879f6b8877752685a966564d07
 $_MODULE['<{universalpay}prestashop>payment_execution_0881a11f7af33bc1b43e437391129d66'] = 'Подтвердите заказ нажатием кнопки \'Подтверждаю заказ\'';
 $_MODULE['<{universalpay}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'Подтверждаю заказ';
 $_MODULE['<{universalpay}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Другие способы оплаты';
+$_MODULE['<{universalpay}prestashop>payment_0881a11f7af33bc1b43e437391129d66'] = 'Подтвердите свой заказ, нажав кнопку \'Подтверждаю заказ\'';
+$_MODULE['<{universalpay}prestashop>payment_46b9e3665f187c739c55983f757ccda0'] = 'Подтверждаю заказ';
